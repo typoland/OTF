b271319e5d4d544c81284ff3dd5cc659b84d1512
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-06-10 11:34:18 +0200

    Initial Commit

---
+++ OTF/OTF.h
@@ -0,0 +1,19 @@
+//
+//  OTF.h
+//  OTF
+//
+//  Created by Łukasz Dziedzic on 10/06/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OTF.
+FOUNDATION_EXPORT double OTFVersionNumber;
+
+//! Project version string for OTF.
+FOUNDATION_EXPORT const unsigned char OTFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTF/PublicHeader.h>
+
+
